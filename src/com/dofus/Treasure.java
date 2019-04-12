1ad3118d85c4c93f9aafbe526813ec031270eb45
NicolasBSN <nicolas.boussenina89@gmail.com>
2019-04-12 15:56:54 +0100

    Update Treasure.java

--- src/com/dofus/Treasure.java
+++ src/com/dofus/Treasure.java
@@ -1,4 +1,14 @@
 package com.dofus;
 
 public class Treasure {
+  // Attributs
+  private int nbPotions, scroll;
+  private double gold;
+        
+   // Constructor
+   public Treasure(int nbPotions, int scroll, double gold) {
+     this.nbPotions = nbPotions;
+     this.scroll = scroll;
+     this.gold = gold;
+   }
 }
