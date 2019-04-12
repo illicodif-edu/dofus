40a85378e43debfba35691610966ad6a214f2cac
NicolasBSN <nicolas.boussenina89@gmail.com>
2019-04-12 16:57:26 +0100

    Update FighterUser.java

--- src/com/dofus/FighterUser.java
+++ src/com/dofus/FighterUser.java
@@ -2,6 +2,7 @@ package com.dofus;
 
 public class FighterUser extends Fighter implements ActionListener {
   
+  // ArrayList of fighters
   private ArrayList<Fighter> fighters;
   
   public Fighter(Weapon weapon, Armor armor, ArrayList<Treasure> treasures, int hitPoints, ArrayList<Fighters> fighters) {
@@ -9,11 +10,12 @@ public class FighterUser extends Fighter implements ActionListener {
       this.fighters = fighters;
    }
   
-  
+  // When we touch a button
   public void actionPerfomed(ActionEvent e){
     
     String actionCommand = e.getActionCommand();
     
+    // Test on the button to trigger the right function
     if(actionCommand.equals("utilize")){
       this.utilizeTreasure();
     }
@@ -28,19 +30,34 @@ public class FighterUser extends Fighter implements ActionListener {
     }
   }
   
+  // To use one of the treasure
   public void utilizeTreasure(){
     
+    
   }
+  
+  // To quit
   public void quit(){
     
   }
   
+  // To fight
   public void fight(){
     
   }
   
+  // To rest
   public void rest(){
+    // 50% chances to regain hp
+    if(Math.random()<0.5){
+      this.setHitPoints(this.getHitPoints+(11 + (int)(Math.random() * ((20 - 11) + 1))));
+    }
     
+    // 50% to lose HP
+    else{
+      this.fight();
+    }
+      
   }
   
   
