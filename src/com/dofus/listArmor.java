253092bbe3daaf6ff7751503cee3bdbbdec37f36
tyu800 <41971264+tyu800@users.noreply.github.com>
2019-04-12 16:41:15 +0100

    Update listArmor.java
    liste termine

--- src/com/dofus/listArmor.java
+++ src/com/dofus/listArmor.java
@@ -1,3 +1,12 @@
-public class ListArmor {
-
-}
+//Armor(String Name, int protection, Boolean isRecuperable,int Type)
+Armor noArmor = new Armor("noArmor",0,false,1);
+Armor noShield = new Armor("noShield",0,false,2);
+Armor leatherArmor = new Armor("Leather Armor",10,true,1);
+Armor chainMail = new Armor("Chain Mail",12,true,1);
+Armor hyde = new Armor("hyde",14,false,1);
+Armor skin = new Armor("skin",10,false,1);
+Armor shield = new Armor("shield",2,true,2);
+Armor NinjaChainMail = new Armor("Ninja Chain Mail",14,true,1);
+Armor scales = new Armor("scales",14,false,1);
+Armor heavyShield = new Armor("Heavy Shield",4,true,2);
+Armor magicAura = new Armor("magic aura",15,false,1);
