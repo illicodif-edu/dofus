aa65cb04fc032761c650adf1b08cc3965c671985
NicolasBSN <nicolas.boussenina89@gmail.com>
2019-04-12 16:34:07 +0100

    Update FighterUser.java
    Add the action Listener

--- src/com/dofus/FighterUser.java
+++ src/com/dofus/FighterUser.java
@@ -1,1 +1,46 @@
+package com.dofus;
 
+public class FighterUser extends Fighter implements ActionListener {
+  
+  public Fighter(Weapon weapon, Armor armor, ArrayList<Treasure> treasures, int hitPoints) {
+      super(weapon, armor, null, treasures, hitPoints);
+   }
+  
+  
+  public void actionPerfomed(ActionEvent e){
+    
+    String actionCommand = e.getActionCommand();
+    
+    if(actionCommand.equals("utilize")){
+      this.utilizeTreasure();
+    }
+    if(actionCommand.equals("rest")){
+      this.rest();
+    }
+    if(actionCommand.equals("quit")){
+      this.quit();
+    }
+    if(actionCommand.equals("fight")){
+      this.rest();
+    }
+  }
+  
+  public void utilizeTreasure(){
+    
+  }
+  public void quit(){
+    
+  }
+  
+  public void fight(){
+    
+  }
+  
+  public void rest(){
+    
+  }
+  
+  
+  
+}  
+  
