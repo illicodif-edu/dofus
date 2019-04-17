1dab1b11fd4d88fff682596b14b548d8598f24ab
NicolasBSN <nicolas.boussenina89@gmail.com>
2019-04-17 17:35:43 +0200

    Update FighterUser.java

--- src/com/dofus/FighterUser.java
+++ src/com/dofus/FighterUser.java
@@ -1,66 +1,48 @@
-package com.dofus;
+import java.util.ArrayList;
 
-public class FighterUser extends Fighter implements ActionListener {
-  
-  // ArrayList of fighters
-  private ArrayList<Fighter> fighters;
-  
-  public Fighter(Weapon weapon, Armor armor, ArrayList<Treasure> treasures, int hitPoints, ArrayList<Fighters> fighters) {
-      super(weapon, armor, null, treasures, hitPoints);
-      this.fighters = fighters;
-   }
-  
-  // When we touch a button
-  public void actionPerfomed(ActionEvent e){
-    
-    String actionCommand = e.getActionCommand();
-    
-    // Test on the button to trigger the right function
-    if(actionCommand.equals("utilize")){
-      this.utilizeTreasure();
-    }
-    if(actionCommand.equals("rest")){
-      this.rest();
-    }
-    if(actionCommand.equals("quit")){
-      this.quit();
-    }
-    if(actionCommand.equals("fight")){
-      this.rest();
-    }
-  }
-  
-  // To use one of the treasure
-  public void utilizeTreasure(){
-    
-    
-  }
-  
-  // To quit
-  public void quit(){
-    
-  }
-  
-  // To fight
-  public void fight(){
-    
-  }
-  
-  // To rest
-  public void rest(){
-    // 50% chances to regain hp
-    if(Math.random()<0.5){
-      this.setHitPoints(this.getHitPoints+(11 + (int)(Math.random() * ((20 - 11) + 1))));
-    }
-    
-    // 50% to lose HP
-    else{
-      this.fight();
-    }
-      
-  }
-  
-  
-  
-}  
+public class FighterUser extends Fighter {
+
+	// ArrayList of fighters
+	private ArrayList<Fighter> fighters;
+
+	public FighterUser(Weapon weapon, Armor armor, Armor armor2, ArrayList<Treasure> treasures, int hitPoints, ArrayList<Fighter> fighters) {
+		super(weapon, armor, null, treasures, hitPoints);
+		this.fighters = fighters;
+	}
+
+	// To use one of the treasure
+	public void utilizeTreasure() {
+		System.out.println("J'utilise une potion");
+
+	}
+
+	// To quit
+	public void quit() {
+		System.out.println("Je quitte la partie");
+		System.exit(0);
+
+	}
+
+	// To fight
+	public void fight() {
+		System.out.println("I fight");
+
+	}
+
+	// To rest
+	public void rest() {
+		System.out.println("Je dors");
+		// 50% chances to regain hp
+		if (Math.random() < 0.5) {
+			this.setHitPoints(this.getHitPoints() + (11 + (int) (Math.random() * ((20 - 11) + 1))));
+		}
+
+		// 50% to lose HP
+		else {
+			this.fight();
+		}
+
+	}
+
+}
   
