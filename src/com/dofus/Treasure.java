ac05574d1ab1783959b0c2900758cc6e5ea1ece5
NicolasBSN <nicolas.boussenina89@gmail.com>
2019-04-19 17:38:18 +0200

    Update Treasure.java

--- src/com/dofus/Treasure.java
+++ src/com/dofus/Treasure.java
@@ -1,15 +1,18 @@
 package com.dofus;
 
+
+
 public class Treasure {
   // Attributs
   private int nbPotions, scroll;
-  private double gold;
+  private double gold, silver;
         
    // Constructor
-   public Treasure(int nbPotions, int scroll, double gold) {
+   public Treasure(int nbPotions, int scroll, double gold, double silver) {
      this.nbPotions = nbPotions;
      this.scroll = scroll;
      this.gold = gold;
+     this.silver = silver;
    }
    public int getNbPotions() {
         return nbPotions;
@@ -22,6 +25,10 @@ public class Treasure {
      public double getGold() {
          return gold;
      }
+     
+     public double getSilver() {
+         return silver;
+     }
 
      public void setNbPotions(int nbPotions) {
          this.nbPotions = nbPotions;
@@ -33,5 +40,9 @@ public class Treasure {
 
      public void setGold(double gold) {
          this.gold = gold;
+     }
+     
+     public void setSilver(double silver) {
+         this.silver = silver;
      }
 }
